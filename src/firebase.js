ad80f488b5316165c81a29fded23ccae157b8a41
Sofie Swagemakers Herou <ss226jn@student.lnu.se>
2026-05-11 16:21:15 +0200

    Remove Firestore related code

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore/lite'
 
 // Firebase project configuration
 const firebaseConfig = {
@@ -15,5 +14,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-export const db = getFirestore(app)
 export const auth = getAuth(app)
